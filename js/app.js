99be4331a674e49dc88772301f9221a3507487ab
Luciano Bertenasco <lbertenasco@gmail.com>
2013-03-12 23:00:49 -0300

    somewhat

--- js/app.js
+++ js/app.js
@@ -2,10 +2,18 @@ window.events = _.clone(Backbone.Events);
 
 var ReadFast = ReadFast || {}
 
+var globalWordIndex = 0;
+var globalTextLength = 0;
+var timeToGo = 0;
+var globalTimeSpan =500;
+
+
 ReadFast.Visor = Backbone.View.extend({
 
   events: {
-    'click button': 'start'
+    'click #play': 'start',
+    'click #pause': 'stop',
+    'click #rewind': 'rewind',
   },
   
   initialize: function () {
@@ -20,37 +28,70 @@ ReadFast.Visor = Backbone.View.extend({
     events.on('count', this.count, this);
   },
 
-  cycle: function (wordIndex) {
-    $('output', this.$el).text(this.words[wordIndex]);
-    wordIndex ++;
+  cycle: function () {
+    if (this.countable) {
+      $('output', this.$el).text(this.words[globalWordIndex]);
+      $('#progress').css('width', (globalWordIndex / globalTextLength)*100 + '%' );
+      $('#rest').text(Math.round((globalTextLength - globalWordIndex ) * globalTimeSpan / 1000) + "s");
+      globalWordIndex ++;
 
-    if (wordIndex < this.words.length) {
-      setTimeout(
-        "events.trigger('cycle'," + wordIndex + ")", 
-        this.timeSpan
-      );      
-    } else {
-      this.countable = false;
+      if (globalWordIndex < this.words.length) {
+        setTimeout(
+          "events.trigger('cycle'," + globalWordIndex + ")", 
+          this.timeSpan
+        );      
+      } else {
+        this.countable = false;
+      }
     }
   },
 
   start: function () {
-    this.countable = true;
-    this.countSoFar = 0;
-    $("kbd").text(this.countSoFar / 1000);
-    this.text = $.trim($("textarea").val());
-    this.words = this.text.split(" ");
-    setTimeout("events.trigger('count')", 10);
-    events.trigger('cycle', 0);
+    if (!this.countable) {
+      this.countable = true;
+      this.countSoFar = globalWordIndex;
+      $("#time").text(Math.round(this.countSoFar / 1000) + 's');
+      this.text = $.trim($("textarea").val());
+      this.words = this.text.split(/\s/);
+      globalTextLength = this.words.length;
+      setTimeout("events.trigger('count')", 10);
+      $('#textarea1').addClass('hide');
+      $('#play').addClass('hide');
+      events.trigger('cycle');
+    }
+  },
+
+  stop: function () {
+    if (this.countable) {
+      this.countable = false;
+      $('#pause').text('Play');
+    }else{
+      this.countable = true;
+      $('#pause').text('Pausa');
+      $("#time").text(Math.round(this.countSoFar / 1000) + 's');
+      setTimeout("events.trigger('count')", 10);
+      events.trigger('cycle', globalWordIndex - 1);
+    }
+  },
+
+  rewind: function () {
+    if (globalWordIndex > 11){
+      globalWordIndex = globalWordIndex - 10;
+    }else{
+      globalWordIndex = 0;
+    }
+    $('#progress').css('width', (globalWordIndex / globalTextLength)*100 + '%' );
   },
 
   updateTimeSpan: function (timeSpan) {
     this.timeSpan = timeSpan;
+    globalTimeSpan = timeSpan;
+    $('#rest').text(Math.round((globalTextLength - globalWordIndex ) * globalTimeSpan / 1000) + "s");
   },
 
   count: function () {
     this.countSoFar += 10;
-    $("kbd").text(this.countSoFar / 1000);
+    $("#time").text(Math.round(this.countSoFar / 1000) + 's');
     if (this.countable) {
       setTimeout("events.trigger('count')", 10);
     }
